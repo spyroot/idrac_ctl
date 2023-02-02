f095a3fb1453c0c29050bb5e4b6d3b63f4a59a87
spyroot <spyroot@gmail.com>
2023-02-02 08:21:12 +0400

    moved examples

--- examples/exameple_volume_setup.sh
+++ examples/exameple_volume_setup.sh
@@ -1,2 +1,2 @@
 # initialize
-python idrac_ctl.py volume-init --dev_id AHCI.Integrated.1-1 --vol_id Disk.Direct.0-0:AHCI.Integrated.1-1
+idrac_ctl volume-init --dev_id AHCI.Integrated.1-1 --vol_id Disk.Direct.0-0:AHCI.Integrated.1-1
