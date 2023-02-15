b9b7bbdae512efea6a77290d2948d77d4b5216d7
spyroot <spyroot@gmail.com>
2023-02-15 23:26:43 +0100

    unit test

--- tests/test_cmd_accounts.py
+++ tests/test_cmd_accounts.py
@@ -4,6 +4,10 @@ from unittest import TestCase
 from idrac_ctl.idrac_manager import IDracManager, CommandResult
 from idrac_ctl.idrac_shared import ApiRequestType
 from idrac_ctl.redfish_shared import RedfishJson
+import logging
+
+logging.basicConfig()
+log = logging.getLogger("LOG")
 
 
 class TestAccounts(TestCase):
@@ -29,7 +33,7 @@ class TestAccounts(TestCase):
         """
         manager = self.setUpClass()
         query_result = manager.sync_invoke(
-            ApiRequestType.QueryAccounts, "attribute_inventory")
+            ApiRequestType.QueryAccounts, "query_accounts")
 
         self.assertIsInstance(query_result, CommandResult)
         self.assertIsInstance(query_result.data, dict)
@@ -51,7 +55,7 @@ class TestAccounts(TestCase):
         """
         manager = self.setUpClass()
         query_result = manager.sync_invoke(
-            ApiRequestType.QueryAccounts, "attribute_inventory")
+            ApiRequestType.QueryAccounts, "query_accounts", do_expanded=True)
 
         self.assertIsInstance(query_result, CommandResult)
         self.assertIsInstance(query_result.data, dict)
@@ -63,51 +67,44 @@ class TestAccounts(TestCase):
         self.assertTrue(RedfishJson.Members in query_result.data,
                         f"Failed to fetch mandatory {RedfishJson.Members} key")
 
-        members_data = query_result.data[RedfishJson.Members]
-        self.assertTrue(RedfishJson.Members in members_data,
+        self.assertTrue(RedfishJson.Members in query_result.data,
                         f"Failed to fetch mandatory {RedfishJson.Members} "
                         f"key from member data")
+
+        self.assertTrue(RedfishJson.MembersCount in query_result.data,
+                        f"Failed to fetch mandatory {RedfishJson.MembersCount} "
+                        f"key from member data")
+
+        log.warning(f"Memer data{query_result.data[RedfishJson.MembersCount]}")
+        member_count = int(query_result.data[RedfishJson.MembersCount])
 
         members_data = query_result.data[RedfishJson.Members]
         self.assertIsInstance(members_data, list)
 
-        self.assertTrue(RedfishJson.MembersCount in members_data,
-                        f"Failed to fetch mandatory {RedfishJson.MembersCount} "
-                        f"key from member data")
-
-        member_count = int(RedfishJson.MembersCount)
         self.assertTrue(len(members_data) == member_count,
                         f"member count and len of list must match")
-    #
-    # def test_basic_base_expanded_query(self):
-    #     """test basic query
-    #     :return:
-    #     """
-    #     manager = self.setUpClass()
-    #     query_result = manager.sync_invoke(
-    #         ApiRequestType.QueryAccounts, "attribute_inventory", filename="")
-    #
-    #     self.assertIsInstance(query_result, CommandResult)
-    #     self.assertIsInstance(query_result.data, dict)
-    #     try:
-    #         json.dumps(query_result.data, sort_keys=True, indent=4)
-    #     except Exception as _:
-    #         self.fail("raised exception")
-    #
-    #     self.assertTrue(RedfishJson.Members in query_result.data,
-    #                     f"Failed to fetch mandatory {RedfishJson.Members} key")
-    #
-    #     members_data = query_result.data[RedfishJson.Members]
-    #     self.assertTrue(RedfishJson.Members in members_data,
-    #                     f"Failed to fetch mandatory {RedfishJson.Members} key from member data")
-    #
-    #     members_data = query_result.data[RedfishJson.Members]
-    #     self.assertIsInstance(members_data, list)
-    #
-    #     self.assertTrue(RedfishJson.MembersCount in members_data,
-    #                     f"Failed to fetch mandatory {RedfishJson.MembersCount} key from member data")
-    #
-    #     member_count = int(RedfishJson.MembersCount)
-    #     self.assertTrue(len(members_data) == member_count,
-    #                     f"member count and len of list must match")
 
+        for m in members_data:
+            self.assertTrue("AccountTypes" in m, f"Failed to fetch mandatory AccountTypes " f"key from member data")
+
+    def test_basic_base_save_expanded_query(self):
+        """test basic query
+        :return:
+        """
+        log.warning(f"Running test_basic_base_save_expanded_query")
+
+        manager = self.setUpClass()
+        query_result = manager.sync_invoke(
+            ApiRequestType.QueryAccounts, "query_accounts",
+            do_expanded=True,
+            filename="/tmp/account_expanded.query.json")
+
+        self.assertIsInstance(query_result, CommandResult)
+        self.assertIsInstance(query_result.data, dict)
+        try:
+            json.dumps(query_result.data, sort_keys=True, indent=4)
+        except Exception as _:
+            self.fail("raised exception")
+
+        self.assertTrue(RedfishJson.Members in query_result.data,
+                        f"Failed to fetch mandatory {RedfishJson.Members} key")
